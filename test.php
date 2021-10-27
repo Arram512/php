3d36edbfa6841698c0de7e02462bbf006f74853e
Eteriass <theplaguedoctoreteriass@gmail.com>
2021-10-27 09:56:14 +0400

    2

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+    $name = $_POST['name'];
+    $sName = $_POST['sname'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    echo "Բարև ձեզ, {$name} {$sName}։ Դուք {$age} տարեկան եք, ձեր էլ-փոստի հասցեն է ՝ {$email}";
+?>
